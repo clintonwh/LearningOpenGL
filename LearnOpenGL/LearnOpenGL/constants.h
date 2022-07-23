dfdfd8c36657af5ecbf22b510d292e23bbfaf32d
clintonwh <clintonw@tcd.ie>
2022-07-24 00:28:52 +0900

    commit code

---
+++ LearnOpenGL/LearnOpenGL/constants.h
@@ -0,0 +1,21 @@
+//
+//  constants.h
+//  LearnOpenGL
+//
+//  Created by William Clinton on 16/07/2022.
+//
+
+#ifndef constants_h
+#define constants_h
+
+//ShaderConstants
+    const char* vertexShaderPath = "/Users/williamclinton/Xcode/LearnOpenGL/Shaders/basic.vs";
+    const char* fragmentShaderPath = "/Users/williamclinton/Xcode/LearnOpenGL/Shaders/basic.fs";
+
+//TextureConstants
+    const char* wallTexturePath = "/Users/williamclinton/Xcode/LearnOpenGL/Textures/wall.jpeg";
+    const char* containerTexturePath = "/Users/williamclinton/Xcode/LearnOpenGL/Textures/container.jpeg";
+    const char* awesomefaceTexturePath = "/Users/williamclinton/Xcode/LearnOpenGL/Textures/awesomeface.png";
+
+
+#endif /* constants_h */
