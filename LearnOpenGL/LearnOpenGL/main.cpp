81e34e722716dc5cdafde4e5432f5fb9f55470ce
clintonwh <clintonw@tcd.ie>
2022-09-24 18:07:50 +0900

    move imgui files into group folder - adding the files in an existing folder was the issue

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -1,9 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
